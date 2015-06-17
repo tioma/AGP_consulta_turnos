6d924e4c84b092d3bf9ba1469bfc5bdc7ae21f65
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2015-06-17 16:53:39 -0300

    Campo de mail en minúsculas y campo de contenedor en mayusculas

--- directives/toUpper.js
+++ directives/toUpper.js
@@ -1,22 +1,26 @@
 /**
  * Created by artiom on 10/06/15.
  */
-myapp.directive('toupper', function() {
+myapp.directive('uppercased', function() {
 	return {
 		require: 'ngModel',
 		link: function(scope, element, attrs, modelCtrl) {
-			var mayusculas = function(inputValue) {
-				if (inputValue != undefined && inputValue != ''){
-					var capitalized = inputValue.toUpperCase();
-					if(capitalized !== inputValue) {
-						modelCtrl.$setViewValue(capitalized);
-						modelCtrl.$render();
-					}
-					return capitalized;
-				}
-			};
-			modelCtrl.$parsers.push(mayusculas);
-			mayusculas(scope[attrs.ngModel]);  // capitalize initial value
+			modelCtrl.$parsers.push(function(input) {
+				return input ? input.toUpperCase() : "";
+			});
+			element.css("text-transform","uppercase");
+		}
+	};
+});
+
+myapp.directive('lowercased', function() {
+	return {
+		require: 'ngModel',
+		link: function(scope, element, attrs, modelCtrl) {
+			modelCtrl.$parsers.push(function(input) {
+				return input ? input.toLowerCase() : "";
+			});
+			element.css("text-transform","lowercase");
 		}
 	};
 });
